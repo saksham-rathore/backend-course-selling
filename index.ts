aa1faf9f3a7475e6395ae9084121f8000e8e7de4
saksham762007 <samrathore07062007@gmail.com>
2026-08-15 20:05:02 +0530

    feat: update Prisma schema and add new models; modify package.json for tsx support

--- index.ts
+++ index.ts
@@ -31,5 +31,5 @@ app.get('/courses', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`your app is listening on port ${port}`);
 });
